a0f041771a662460ffe3d5a5c90683edeb49895b
[magicscottish] <[akaprometheus@gmail.com]>
2020-12-09 10:38:43 -0800

    Added heroku url to ALLOWED_HOSTS

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -28,7 +28,9 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '+nfz7k0@440n_bn=((5(j7h0#2x&)8
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    cryptic-meadow-03490.herokuapp.com,
+]
 
 
 # Application definition
